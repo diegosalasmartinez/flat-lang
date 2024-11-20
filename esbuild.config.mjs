85e867057bb559919a3379ceaa9122f18bb0296e
diegosalasmartinez <diesalasmart@gmail.com>
2024-11-19 23:50:18 -0500

    feat: update dependencies and rename esbuild.config.mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,10 +1,17 @@
 import esbuild from "esbuild";
+import fs from "fs";
+
+const entryPoints = ["./dist/styles.css"];
+if (fs.existsSync("./dist/scripts.js")) {
+    entryPoints.push("./dist/scripts.js");
+}
 
 esbuild.build({
-    entryPoints: ["./dist/styles.css", "./dist/scripts.js"],
+    entryPoints,
     outdir: "./dist",
     minify: true,
     bundle: true,
+    allowOverwrite: true
 }).then(() => {
     console.log("Assets optimized successfully!");
 }).catch((error) => {
